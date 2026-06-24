48898ced78a9526b7d69a6930a3536e8602ed2b1
Shivank <inshivank027@gmail.com>
2026-06-24 13:19:48 +0530

    Revert "contact feature update"
    This reverts commit d254612d9233afc9adf45155b78664262a787ae6.

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -29,12 +29,6 @@ const itemSchema = new mongoose.Schema({
     type: [String],
     default: []
   },
-  whatsappNumber: {
-    type: String,
-    trim: true,
-    maxlength: [20, "WhatsApp number is too long"],
-    default: ""
-  },
   seller: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
